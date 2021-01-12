602f233db0f70883699095258a84f72242bd07d2
Alex Molas <a.molasmartin@stuart.com>
2021-01-12 14:37:31 +0100

    add print parenthesis method

--- matrix-multiplication/mat_mult/utils.py
+++ matrix-multiplication/mat_mult/utils.py
@@ -1,8 +1,36 @@
-def memoize(f):
-    memo = {}
+import random
+from typing import Sequence
 
-    def helper(dims, i, j):
-        if (i, j) not in memo:
-            memo[(i, j)] = f(dims, i, j)
-        return memo[(i, j)]
-    return helper
+from mat_mult.mcm import memoized_mcm
+
+
+def generate_test_cases(n_tests: int, min_len: int, max_len: int, min_dim: int, max_dim: int) \
+        -> Sequence[Sequence[int]]:
+    """
+
+    :param n_tests: number of test to generate
+    :param min_len: minimum number of matrices for each test case
+    :param max_len: maximum number of matrices for each test case
+    :param min_dim: minimum dimension for each matrix (applies both for rows and columns)
+    :param max_dim: maximum dimension for each matrix (applies both for rows and columns)
+    :return:
+    """
+    solutions = []
+    for n in range(n_tests):
+        test_len = random.randint(min_len, max_len)
+        dims = tuple([random.randint(min_dim, max_dim) for _ in range(test_len)])
+        solution = memoized_mcm(dims=dims)[0]
+        solutions.append([dims, solution])
+    return solutions
+
+
+def print_parenthesis(i, j, s):
+    if i == j:
+        name = chr(65 + i)
+        print(name, end="")
+        return
+    else:
+        print("(", end="")
+        print_parenthesis(i, s[i][j] - 1, s)
+        print_parenthesis(s[i][j], j, s)
+        print(")", end="")
