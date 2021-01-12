7a8b9edcae756af794c6070bf7d24bfd70d89503
Alex Molas <a.molasmartin@stuart.com>
2021-01-12 14:38:57 +0100

    refactor from classes to methods

--- matrix-multiplication/mat_mult/mcm.py
+++ matrix-multiplication/mat_mult/mcm.py
@@ -1,63 +1,49 @@
-from typing import Tuple
+from typing import Tuple, Sequence
 
-from mat_mult.base import MatrixChainMultiplication
-from mat_mult.utils import memoize
-
-
-class MCMNaive(MatrixChainMultiplication):
-    def get_best_order(self, dims: Tuple[int]) -> str:
-        ...
-
-    def get_min_cost(self, dims: Tuple[int]) -> int:
-
-        def _get_min_cost(dims_, i, j):
-            """
-            :param dims_: an array with the matrices dimensions
-            :param i: initial index
-            :param j: final index
-            i and j define the subsequence of dims_ from which do we want to know the best ordering
-            :return:
-            """
-            # base case, when we only have one matrix
-            if j <= i + 1:
-                return 0
-
-            min_cost = float('inf')
-
-            for k in range(i + 1, j):
-                cost = _get_min_cost(dims_, i, k) + _get_min_cost(dims_, k, j) + dims_[i] * dims_[k] * dims_[j]
-                if cost < min_cost:
-                    min_cost = cost
-            return min_cost
-        return _get_min_cost(dims, 0, len(dims) - 1)
+from mat_mult.decorators import memoize
 
 
-class MCMMemoization(MatrixChainMultiplication):
-    def get_best_order(self, dims: Tuple[int]) -> str:
-        ...
+def naive_mcm(dims: Tuple[int]) -> Tuple[int, Sequence]:
+    n_matrices = len(dims) - 1
+    s = [[0 for _ in range(n_matrices)] for _ in range(n_matrices)]
 
-    def get_min_cost(self, dims: Tuple[int]) -> int:
-        @memoize
-        def _get_min_cost(dims_, i, j):
-            """
-            :param dims_: an array with the matrices dimensions
-            :param i: initial index
-            :param j: final index
-            i and j define the subsequence of dims_ from which do we want to know the best ordering
-            :return:
-            """
-            # base case, when we only have one matrix
-            if j <= i + 1:
-                return 0
+    def _mcm(d, i, j):
+        if j <= i + 1:
+            return 0
 
-            min_cost = float('inf')
+        min_cost = float('inf')
 
-            for k in range(i + 1, j):
-                cost = _get_min_cost(dims_, i, k) + \
-                       _get_min_cost(dims_, k, j) \
-                       + dims_[i] * dims_[k] * dims_[j]
-                if cost < min_cost:
-                    min_cost = cost
-            return min_cost
+        best_partition = None
 
-        return _get_min_cost(dims, 0, len(dims) - 1)
+        for k in range(i + 1, j):
+            cost = _mcm(d, i, k) + _mcm(d, k, j) + d[i] * d[k] * d[j]
+            if cost < min_cost:
+                min_cost = cost
+                best_partition = k
+        s[i][j - 1] = best_partition
+        return min_cost
+    return _mcm(dims, 0, n_matrices), s
+
+
+def memoized_mcm(dims: Tuple[int]) -> Tuple[int, Sequence]:
+    n_matrices = len(dims) - 1
+    s = [[0 for _ in range(n_matrices)] for _ in range(n_matrices)]
+
+    @memoize
+    def _mcm(d, i, j):
+        if j <= i + 1:
+            return 0
+
+        min_cost = float('inf')
+
+        best_partition = None
+
+        for k in range(i + 1, j):
+            cost = _mcm(d, i, k) + _mcm(d, k, j) + d[i] * d[k] * d[j]
+            if cost < min_cost:
+                min_cost = cost
+                best_partition = k
+        s[i][j - 1] = best_partition
+        return min_cost
+
+    return _mcm(dims, 0, n_matrices), s
